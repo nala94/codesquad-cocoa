cbd80031edbcc5073d51ae29174a519f26b80a02
Lee jaegwang <76624387+nala94@users.noreply.github.com>
2021-11-04 03:53:41 +0900

    Refactor: printMember() 메소드
    main 메소드를 간소화 하기 위해 MEMBER 배열을
    메소드 밖에서 선언하고 객체 생성을 printMember()
    안에서 해주었다

--- src/day3/GetRandomMember.java
+++ src/day3/GetRandomMember.java
@@ -4,29 +4,26 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GetRandomMember {
+    public static final String[] MEMBER = {"BC", "호두마루", "DAVE", "Barney", "meatsby", "mong",
+            "Jun", "Phil", "Dike", "Jhin", "OS"};
 
-    public void printMember(Random rd, String[] member, int n){
-        for(int i=0; i<n; i++){
-            int x = rd.nextInt(member.length);
-            System.out.print(member[x]);
-            if(i < n-1){
-                System.out.print(", ");
-            }
-
-        }
-    }
-
-    public static void main(String[] args) {
+    public void printMember() {
         Random rd = new Random();
         Scanner sc = new Scanner(System.in);
         System.out.print("몇 명인가요? ");
         int n = sc.nextInt();
-
-        String[] member = {"BC", "호두마루", "DAVE", "Barney", "meatsby", "mong", "Jun", "Phil", "Dike", "Jhin", "OS"};
-        GetRandomMember grm = new GetRandomMember();
-
-        grm.printMember(rd, member, n);
-
-
+        for (int i = 0; i < n; i++) {
+            int x = rd.nextInt(MEMBER.length);
+            System.out.print(MEMBER[x]);
+            if (i < n - 1) {
+                System.out.print(", ");
+            }
+        }
     }
+
+    public static void main(String[] args) {
+        GetRandomMember grm = new GetRandomMember();
+        grm.printMember();
+    }
+
 }
