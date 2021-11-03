b11f74873f05aff2195c82a220b08b67170ff244
Lee jaegwang <76624387+nala94@users.noreply.github.com>
2021-11-03 15:19:00 +0900

    Init GetRandomMember

---
+++ src/day3/GetRandomMember.java
@@ -0,0 +1,28 @@
+package day3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GetRandomMember {
+
+    public void printMember(Random rd, String[] member, int n){
+        for(int i=0; i<n; i++){
+            int x = rd.nextInt(member.length);
+            System.out.print(member[x] +", ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Random rd = new Random();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("몇 명인가요? ");
+        int n = sc.nextInt();
+
+        String[] member = {"BC", "호두마루", "DAVE", "Barney", "meatsby", "mong", "Jun", "Phil", "Dike", "Jhin", "OS"};
+        GetRandomMember grm = new GetRandomMember();
+
+        grm.printMember(rd, member, n);
+
+
+    }
+}
