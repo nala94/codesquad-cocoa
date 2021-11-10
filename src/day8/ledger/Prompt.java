68a9bcafffe58b758f383f3e60f3b065a982e594
Lee jaegwang <76624387+nala94@users.noreply.github.com>
2021-11-10 12:02:28 +0900

    데이터 입력, 데이터 출력 메소드 생성

--- src/day8/ledger/Prompt.java
+++ src/day8/ledger/Prompt.java
@@ -1,19 +1,54 @@
 package day8.ledger;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Prompt {
-    void runPrompt(){
-        Scanner sc = new Scanner(System.in);
-        HashMap<String, String> user = new HashMap<>();
+    Scanner sc = new Scanner(System.in);
+    AccountInfo accInfo;
+    ArrayList<AccountInfo> acc;
+    HashMap<String, String> user;
+
+    void runPrompt() {
+        acc = new ArrayList<>();
+        user = new HashMap<>();
+
+
         System.out.print("Name: ");
         String name = sc.next();
         System.out.print("Password: ");
         String pw = sc.next();
 
         user.put(name, pw);
+        InputData();
+        printData();
+    }
 
+    private void printData() {
+        for (int i=0; i < acc.size(); i++){
+            accInfo = acc.get(i);
+
+            System.out.println(accInfo.inputDate);
+            System.out.println(accInfo.description);
+            System.out.println(accInfo.income);
+            System.out.println(accInfo.outcome);
+        }
+    }
+
+    private void InputData() {
+        accInfo = new AccountInfo();
+        sc.nextLine();
+
+        System.out.print("날짜 : ");
+        accInfo.inputDate = sc.nextLine();
+        System.out.print("내용 : ");
+        accInfo.description = sc.nextLine();
+        System.out.print("수입 : ");
+        accInfo.income = sc.nextInt();
+        System.out.print("지출 : ");
+        accInfo.outcome = sc.nextInt();
+        acc.add(accInfo);
     }
 }
