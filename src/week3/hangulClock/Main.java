3f7cbbbd6477b2191f986b4514effe07d8009afd
Lee jaegwang <76624387+nala94@users.noreply.github.com>
2021-11-17 22:38:44 +0900

    명령어를 입력받아 시계 또는 달력을 출력

--- src/week3/hangulClock/Main.java
+++ src/week3/hangulClock/Main.java
@@ -1,9 +1,21 @@
 package week3.hangulClock;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         HangulClock h = new HangulClock();
-        h.runTimer();
+        Calendar c = new Calendar();
+        Scanner s = new Scanner(System.in);
+        System.out.println("(clock: 시계 출력, cal: 달력 출력)");
+        System.out.print(" > ");
+        String cmd = s.next();
+        if(cmd.equals("clock")){
+            h.runTimer();
+        }
+        if(cmd.equals("cal")){
+            c.runCalendar();
+        }
 
     }
 }
