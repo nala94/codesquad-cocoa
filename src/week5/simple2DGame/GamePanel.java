c4394d29fe7a493e5233e13ff26ab64480f78cad
Lee jaegwang <76624387+nala94@users.noreply.github.com>
2021-11-30 06:19:33 +0900

    스레드와 키보드 입력 기능 추가

--- src/week5/simple2DGame/GamePanel.java
+++ src/week5/simple2DGame/GamePanel.java
@@ -2,8 +2,9 @@ package week5.simple2DGame;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyListener;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements Runnable {
 
     // SCREEN SETTINGS
     final int originalTileSize = 16; //16x16 tile
@@ -15,10 +16,69 @@ public class GamePanel extends JPanel{
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
-    public GamePanel(){
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+
+    // Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        while (gameThread != null) {
+//            System.out.println("The game loop is running");
+
+            // 1 UPDATE: update information such as character position
+            update();
+
+            // 2 DRAW: draw the screen with the updated information
+            repaint();
+        }
+
+
+    }
+
+    public void update() {
+
+        if (keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        } else if (keyH.downPressed == true) {
+            playerY += playerSpeed;
+        } else if (keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        } else if (keyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
     }
 }
