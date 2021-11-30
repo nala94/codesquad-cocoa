3028026cee226cce9158f61239a5c20345f3b1b0
Lee jaegwang <76624387+nala94@users.noreply.github.com>
2021-11-30 22:03:47 +0900

    Refac: run()을 다른 로직으로 변경

--- src/week5/simple2DGame/GamePanel.java
+++ src/week5/simple2DGame/GamePanel.java
@@ -44,35 +44,38 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
 
-        double drawInterval = 1000000000 / FPS;
-        double nextDrawTime = System.nanoTime() + drawInterval;
+        double drawInterval = 1000_000_000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currenTime;
+        long timer = 0;
+        int drawCount = 0;
 
         while (gameThread != null) {
 
+            currenTime = System.nanoTime();
 
-            // 1 UPDATE: update information such as character position
-            update();
+            delta += (currenTime - lastTime) / drawInterval;
+            timer += (currenTime - lastTime);
 
-            // 2 DRAW: draw the screen with the updated information
-            repaint();
+            lastTime = currenTime;
 
-
-            try {
-                double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime /= 1000000;
-
-                if (remainingTime < 0) {
-                    remainingTime = 0;
-                }
-                Thread.sleep((long) remainingTime);
-
-                nextDrawTime += drawInterval;
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
             }
+
+            if(timer >= 1000_000_000){
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+
         }
     }
+
 
     public void update() {
 
