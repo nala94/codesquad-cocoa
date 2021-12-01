e30689ca58143b13b6a2bd12c6136e27632c925c
Lee jaegwang <76624387+nala94@users.noreply.github.com>
2021-12-02 00:38:13 +0900

    text 파일에서 맵 불러오기 구현

--- src/week5/simple2DGame/tile/TileManager.java
+++ src/week5/simple2DGame/tile/TileManager.java
@@ -4,21 +4,25 @@ import week5.simple2DGame.main.GamePanel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
 
 public class TileManager {
 
     GamePanel gp;
     Tile[] tile;
+    int[][] mapTileNum;
 
-    public TileManager(GamePanel gp){
+    public TileManager(GamePanel gp) {
         this.gp = gp;
 
         tile = new Tile[10];
+        mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
 
         getTileImage();
+        loadMap("src/week5/simple2DGame/res/maps/map01.txt");
     }
 
-    public void getTileImage(){
+    public void getTileImage() {
 
         tile[0] = new Tile();
         tile[0].image = new ImageIcon("src/week5/simple2DGame/res/tiles/grass.png").getImage();
@@ -31,8 +35,63 @@ public class TileManager {
 
     }
 
-    public void draw(Graphics2D g2){
+    public void loadMap(String filePath) {
 
-        g2.drawImage(tile[0].image, 0, 0, gp.tileSize, gp.tileSize, null);
+        try {
+            File file = new File(filePath);
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            int col = 0;
+            int row = 0;
+
+            while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+
+                String line = br.readLine();
+
+                while (col < gp.maxScreenCol) {
+
+                    String[] numbers = line.split(" ");
+
+                    int num = Integer.parseInt(numbers[col]);
+
+                    mapTileNum[col][row] = num;
+                    col++;
+
+                }
+                if (col == gp.maxScreenCol) {
+                    col = 0;
+                    row++;
+                }
+
+            }
+            br.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void draw(Graphics2D g2) {
+
+        int col = 0;
+        int row = 0;
+        int x = 0;
+        int y = 0;
+
+        while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+
+            int tileNum = mapTileNum[col][row];
+
+            g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
+            col++;
+            x += gp.tileSize;
+
+            if (x == gp.screenWidth) {
+                col = 0;
+                x = 0;
+                row++;
+                y += gp.tileSize;
+            }
+        }
     }
 }
