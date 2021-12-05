d713ae4da56847f2479017425e0274857b50f0fb
Lee jaegwang <76624387+nala94@users.noreply.github.com>
2021-12-05 21:58:50 +0900

    랜덤하게 세자리 숫자를 생성

--- src/bullsAndCows/Main.java
+++ src/bullsAndCows/Main.java
@@ -1,7 +1,20 @@
 package bullsAndCows;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            numbers.add(i);
+        }
+
+        Collections.shuffle(numbers);
+        int answer = numbers.get(0) * 100 +
+                numbers.get(1) * 10 +
+                numbers.get(2);
 
     }
 }
